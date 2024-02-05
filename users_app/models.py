332be3c2a7baf0697d8fd1623d8ebf34780680df
Esau Lwanga <31913619+lemndev@users.noreply.github.com>
2024-02-05 12:29:50 +0300

    bootstrap project

---
+++ users_app/models.py
@@ -0,0 +1,40 @@
+"""
+the models for the Users app
+"""
+import uuid
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+# Create your models here.
+class User(AbstractUser):
+    """
+    the user/auth model for
+    """
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+
+    # personal information
+    country_of_origin = models.CharField(max_length=255, null=True, blank=True)
+    first_name = models.CharField(max_length=255, null=True, blank=True)
+    last_name = models.CharField(max_length=255, null=True, blank=True)
+    other_names = models.CharField(max_length=255, null=True, blank=True)
+
+    # contact/auth information
+    # either of these will be used for login
+    # but primarily the phone number shall be considered
+    email = models.EmailField(max_length=255, unique=True)
+    phone_number = models.CharField(max_length=255, null=True, blank=True)
+
+    roles = models.JSONField(null=True, blank=True)
+    prompt_password_change = models.BooleanField(default=False)
+
+    class Meta:
+        """
+        the metadata for the User model
+        """
+        db_table = 'users'
+        ordering = ['username']
