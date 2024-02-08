e0a0c27cfed8b1d1b056b5a4cd634a304668cc2e
Esau Lwanga <31913619+lemndev@users.noreply.github.com>
2024-02-08 15:18:32 +0300

    implementation to check for required information

---
+++ misc_app/controllers/check_required_information.py
@@ -0,0 +1,32 @@
+"""
+check that all required details are provided
+"""
+
+
+def check_required_information(
+    required_information: list,
+    provided_information: dict
+) -> dict:
+    """
+    compare the provided information to the required information
+    """
+    provided_information = dict(provided_information)
+    for attribute in required_information:
+        key = attribute.get('key')
+        label = attribute.get('label')
+        minimum_length = attribute.get('minimum_length')
+        if key not in provided_information.keys():
+            return {
+                "status": False,
+                "message": f"{label} is required"
+            }
+
+        if len(provided_information[key].strip()) < minimum_length:
+            return {
+                "status": False,
+                "message": f"{label} must be at least {minimum_length} characters long"
+            }
+
+    return {
+        "status": True,
+    }
--- misc_app/tests.py
+++ misc_app/tests.py
@@ -1,3 +1,44 @@
 from django.test import TestCase
+from misc_app.controllers.check_required_information import check_required_information
+
 
 # Create your tests here.
+class MiscAppTestFunctions(TestCase):
+    """
+    the test functions for the Misc app
+    """
+    def test_check_required_information(self):
+        """
+        test the function to check for required information
+        """
+        required_information = [
+            {
+                "key": "key1",
+                "label": "Key 1",
+                "minimum_length": 5
+            },
+            {
+                "key": "key2",
+                "label": "Key 2",
+                "minimum_length": 5
+            }
+        ]
+
+        provided_information1 = {
+            'key1': 'value1',
+        }
+        result = check_required_information(
+            required_information,
+            provided_information1
+        )
+        self.assertEqual(result['status'], False)
+
+        provided_information2 = {
+            'key1': 'value1',
+            'key2': 'value2'
+        }
+        result = check_required_information(
+            required_information,
+            provided_information2
+        )
+        self.assertEqual(result['status'], True)
