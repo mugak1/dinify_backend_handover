b8de6bddeb2992a58807952a1b13c6ba89bf3311
Esau Lwanga <31913619+lemndev@users.noreply.github.com>
2024-11-06 09:15:43 +0300

    delete user account if the employee profile is not created successfully.

--- restaurants_app/controllers/create_employee.py
+++ restaurants_app/controllers/create_employee.py
@@ -3,6 +3,7 @@ from users_app.controllers.self_register import self_register
 from users_app.models import User
 from restaurants_app.serializers import SerializerPutRestaurantEmployee
 from misc_app.controllers.secretary import Secretary
+from django.db import transaction
 
 
 def create_employee(
@@ -14,38 +15,42 @@ def create_employee(
     roles: list,
     creator: User
 ) -> dict:
-    password = User.objects.make_random_password()
-    password = 'password'
+    with transaction.atomic():
+        password = User.objects.make_random_password()
+        password = 'password'
 
-    create_user = self_register(
-        data={
-            'first_name': first_name,
-            'last_name': last_name,
-            'email': email,
-            'phone_number': phone_number,
-            'country': restaurant.country,
-            'password': password
-        },
-        return_user_id=True,
-        send_credential_email=True
-    )
+        create_user = self_register(
+            data={
+                'first_name': first_name,
+                'last_name': last_name,
+                'email': email,
+                'phone_number': phone_number,
+                'country': restaurant.country,
+                'password': password
+            },
+            return_user_id=True,
+            send_credential_email=True
+        )
 
-    if create_user['status'] != 200:
-        return create_user
+        if create_user['status'] != 200:
+            return create_user
 
-    employee_data = {
-        'user': create_user['user_id'],
-        'restaurant': str(restaurant.id),
-        'roles': roles
-    }
-    secretary_args = {
-        'serializer': SerializerPutRestaurantEmployee,
-        'data': employee_data,
-        'required_information': [],
-        'user_id': str(creator.id),
-        'username': creator.username,
-        'success_message': 'The employee has been created successfully. Access credentials have been sent to the user email.',  # noqa
-        'error_message': 'The employee could not be created. Please try again later.'
-    }
-    response = Secretary(secretary_args).create()
-    return response
+        employee_data = {
+            'user': create_user['user_id'],
+            'restaurant': str(restaurant.id),
+            'roles': roles
+        }
+        secretary_args = {
+            'serializer': SerializerPutRestaurantEmployee,
+            'data': employee_data,
+            'required_information': [],
+            'user_id': str(creator.id),
+            'username': creator.username,
+            'success_message': 'The employee has been created successfully. Access credentials have been sent to the user email.',  # noqa
+            'error_message': 'The employee could not be created. Please try again later.'
+        }
+        response = Secretary(secretary_args).create()
+        if response['status'] != 200:
+            # delete the user account that was created
+            User.objects.get(id=create_user['user_id']).delete()
+        return response
