bdabbaa8eddf8add328bff7731e173b83e7c4f35
Esau Lwanga <lemndev@gmail.com>
2024-12-07 19:02:18 +0300

    implementation to disburse momo

--- payment_integrations_app/controllers/yo_integrations.py
+++ payment_integrations_app/controllers/yo_integrations.py
@@ -17,12 +17,7 @@ class YoIntegration:
         self.YO_USERNAME = config('YO_API_USERNAME')
         self.YO_PASSWORD = config('YO_API_PASSWORD')
 
-    def interprete_response(
-        self,
-        request_type: str,
-        request_body: dict,
-        yo_response: str
-    ) -> dict:
+    def interprete_response(self, request_type: str, request_body: dict, yo_response: str) -> dict:
         response_xml_object = ET.fromstring(yo_response.text)
         yo_response_dict = None
         try:
@@ -41,12 +36,7 @@ class YoIntegration:
 
         return yo_response_dict
 
-    def momo_collect(
-        self,
-        transaction_amount: int,
-        msisdn: str,
-        transaction_id: str
-    ) -> bool:
+    def momo_collect(self, transaction_amount: int, msisdn: str, transaction_id: str) -> bool:
         auto_create = ET.Element('AutoCreate')
         request = ET.SubElement(auto_create, 'Request')
         api_username = ET.SubElement(request, 'APIUsername')
@@ -89,10 +79,7 @@ class YoIntegration:
         print(response)
         return True
 
-    def momo_check_transaction(
-        self,
-        yo_transaction_reference: str
-    ) -> bool:
+    def momo_check_transaction(self, yo_transaction_reference: str) -> bool:
         auto_create = ET.Element('AutoCreate')
         request = ET.SubElement(auto_create, 'Request')
         api_username = ET.SubElement(request, 'APIUsername')
@@ -114,6 +101,47 @@ class YoIntegration:
             request_type='momo_check_transaction',
             request_body={'yo_transaction_reference': yo_transaction_reference},
             yo_response=yo_request
+        )
+        print(response)
+        return True
+
+    def momo_disburse(self, transaction_amount: int, msisdn: str, transaction_id: str) -> bool:
+        auto_create = ET.Element('AutoCreate')
+        request = ET.SubElement(auto_create, 'Request')
+        api_username = ET.SubElement(request, 'APIUsername')
+        api_username.text = self.YO_USERNAME
+        api_password = ET.SubElement(request, 'APIPassword')
+        api_password.text = self.YO_PASSWORD
+        method = ET.SubElement(request, 'Method')
+        method.text = 'acwithdrawfunds'
+        non_blocking = ET.SubElement(request, 'NonBlocking')
+        non_blocking.text = 'TRUE'
+        amount = ET.SubElement(request, 'Amount')
+        amount.text = str(transaction_amount)
+        account = ET.SubElement(request, 'Account')
+        account.text = msisdn
+        narrative = ET.SubElement(request, 'Narrative')
+        narrative.text = 'Dinify Disbursement'
+        external_reference = ET.SubElement(request, 'ExternalReference')
+        external_reference.text = transaction_id
+        provider_reference_text = ET.SubElement(request, 'ProviderReferenceText')
+        provider_reference_text.text = 'Dinify Disbursement'
+
+        post_data = ET.tostring(auto_create, xml_declaration=True, encoding='utf-8')
+
+        yo_payment_request = requests.post(
+            API_URL,
+            data=post_data,
+            headers=REQUEST_HEADERS
+        )
+        response = self.interprete_response(
+            request_type='momo_disburse',
+            request_body={
+                'amount': transaction_amount,
+                'msisdn': msisdn,
+                'transaction_id': transaction_id
+            },
+            yo_response=yo_payment_request
         )
         print(response)
         return True
