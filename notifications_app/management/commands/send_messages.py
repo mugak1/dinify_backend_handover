d195afc80a0ca5e482f65cbab7ca8b80a3b62bbe
Esau Lwanga <lemndev@gmail.com>
2025-02-19 09:57:15 +0300

    if a user has a restaurant, check if it is active before sending the credentials

--- notifications_app/management/commands/send_messages.py
+++ notifications_app/management/commands/send_messages.py
@@ -2,6 +2,7 @@
 from django.core.management.base import BaseCommand
 from dinify_backend.mongo_db import MONGO_DB, COL_NOTIFICATIONS
 from notifications_app.controllers.messenger import Messenger
+from restaurants_app.models import Restaurant
 
 
 class Command(BaseCommand):
@@ -17,6 +18,16 @@ class Command(BaseCommand):
 
         notifications = list(notifications)
         for x in notifications:
+            # if the subject is user credentials, check if the user has aany restaurant
+            # if the user is attached to a restaurant, check if it is active
+            if x['subject'] == 'Dinify Credentials!':
+                owner = x['tos'][0]
+                user_restaurants = Restaurant.objects.filter(owner__email=owner).order_by('time_created')
+                if user_restaurants.count() > 0:
+                    restaurant = user_restaurants.first()
+                    if restaurant.status != 'active':
+                        continue
+
             Messenger().send_email(
                 to=x['tos'],
                 cc=x['ccs'],
