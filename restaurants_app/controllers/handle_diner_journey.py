a834de8afa192375b49040cc51d2a6f9551d5f9f
Esau Lwanga <lemndev@gmail.com>
2025-04-02 14:58:24 +0300

    consider string or boolen

--- restaurants_app/controllers/handle_diner_journey.py
+++ restaurants_app/controllers/handle_diner_journey.py
@@ -33,7 +33,7 @@ def handle_show_menu(restaurant_id: str, ignore_approval: str) -> dict:
         'deleted': False
     }
 
-    if ignore_approval == 'true':
+    if ignore_approval in ['true', True] :
     # if ignore_approval is None:
         filters.pop('approved')
         filters.pop('enabled')
